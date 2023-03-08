e5cb42e49e297a1632e79503590a8cc4a1e2606e
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-03-08 10:53:52 -0500

    Cours 11: ajout de la strecture pour la création d'un pligin carrousel

---
+++ carrousel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Extention Carrousel permet d'afficher une boite modale des images d'une gallerie
+ * Package name : Carrousel
+ * Version: 1.0.0
+ */
+/*
+ Plugin name: Carrousel
+ Author: Amine LHANI
+ Plugin URI: https://github.com/E2296101/carrousel
+ Description: Extention Carrousel permet d'afficher une boite modale des images d'une gallerie.
+ */
