c80ed0ed9f784c7de925291d91e5a4be9f1657f2
L'Hani <e2296101@cmaisonneuve.qc.ca>
2023-03-09 15:56:51 -0500

    Cours 12: ajout du css et du code javascript pour afficher et fermer la boite modale

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,6 +1,33 @@
 (function(){
 
-    console.log("test");
+    
+    let bouton_ouvrir = document.querySelector(".bouton__ouvrir");
+    //console.log(bouton_ouvrir.tagName);
+    let elemCarrousel = document.querySelector(".carrousel");
+    let elemBouton_x = document.querySelector(".bouton__x");
+    let elemGalerie = document.querySelector(".galerie");
+    let elemGalerie_img = elemGalerie.querySelectorAll("img");
+    let elmGalerie__figure = document.querySelector('.galerie__figure');
+    console.log(elemGalerie_img.length);
+
+    for(const elmImg of elemGalerie_img ){
+        let elemCarrousel__img = document.createElement('img');
+        elemCarrousel__img.setAttribute('src',elmImg.getAttribute('src'));
+       // elmGalerie__figure.appendChild(elemCarrousel__img);
+    }
+
+    bouton_ouvrir.addEventListener('mousedown',function(){
+
+        console.log('boite ouvrir');
+        elemCarrousel.classList.add('carrousel--ouvrir');
+
+    })
+    elemBouton_x.addEventListener('mousedown',function(){
+
+        console.log('boite fermer');
+        elemCarrousel.classList.remove('carrousel--ouvrir');
+
+    })
 }
 
-)
+)()
